e5d36249a5c2c79c41eda227332b3f615eaf6db4
Eunsuk Kim <eunsukkim84@gmail.com>
2023-12-22 15:40:52 +0900

    ㄹ

--- src/main/java/com/example/miniprogect1/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.miniprogect1.repository;
-
-
-import com.example.miniprogect1.domain.UserDotori;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<UserDotori, Integer> {
-
-}
--- src/main/java/com/example/miniprogect1/repository/VisitCountRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.miniprogect1.repository;
-
-import com.example.miniprogect1.domain.User;
-import com.example.miniprogect1.domain.VisitCount;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface VisitCountRepository extends JpaRepository<VisitCount, Long> {
-    VisitCount findByUser(User user);
-}
