8c7d0081b5189b8d045eca21545c0b077e15943b
Eunsuk Kim <eunsukkim84@gmail.com>
2023-12-22 18:17:03 +0900

    ㄹ

---
+++ src/main/java/com/example/junk/JunkApplication.java
@@ -0,0 +1,13 @@
+package com.example.junk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JunkApplication {
+//ㅂㄷㅈㄹ
+	public static void main(String[] args) {
+		SpringApplication.run(JunkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/junk/JunkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.junk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JunkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
