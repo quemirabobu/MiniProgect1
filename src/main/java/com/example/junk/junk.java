805c553994898ece9fe97a44f4280550f2f4bc1a
Eunsuk Kim <eunsukkim84@gmail.com>
2023-12-26 15:36:11 +0900

    자

---
+++ src/main/java/com/example/junk/junk.java
@@ -0,0 +1,17 @@
+package com.example.junk;
+
+/**
+ * @프로젝트명        : MiniProgect1
+ * @클래스명        : junk
+ * @패키지 정보  : com.example.junk
+ * @설명        :
+ * @작성자 eunsukkim
+ * @작성일        : 26/12/23 3:00 PM
+ * @version v.0.1
+ */
+public class junk {
+
+	public static void main(String[] args) {
+		System.out.println("hahahah");
+	}
+}
